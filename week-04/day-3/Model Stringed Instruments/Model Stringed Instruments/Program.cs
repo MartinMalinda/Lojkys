b64276a653e0ccc9432b08f2d172c65b7a1e779c
Lukas Lojka <lojkys@gmail.com>
2019-10-02 15:25:14 +0200

    added Instrument task with changed namespace

--- week-04/day-3/Model Stringed Instruments/Model Stringed Instruments/Program.cs
+++ week-04/day-3/Model Stringed Instruments/Model Stringed Instruments/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model_Stringed_Instruments;
 
-namespace Model_Stringed_Instruments
+namespace ConcertHall
 {
     class Program
     {
