6cfda2d48548b6c32564c11585729486b73e9828
Lukas Lojka <lojkys@gmail.com>
2019-10-02 20:34:58 +0200

    added Reservations task

---
+++ week-04/day-3/Reservations/Reservations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+
+            //List<Reservation> reservations = new List<Reservation>();
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            //reservations.Add(new Reservation());
+            Console.WriteLine();
+            Reservation reservation1 = new Reservation();
+            Reservation reservation2 = new Reservation();
+            Reservation reservation3 = new Reservation();
+            Reservation reservation4 = new Reservation();
+
+            reservation1.MyReservation();
+            reservation2.MyReservation();
+            reservation3.MyReservation();
+            reservation4.MyReservation();
+            Console.WriteLine();
+
+            //foreach (var reservation in reservations)
+            //{
+            //    reservation.MyReservation();
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/day-3/Reservations/Reservations/Reservation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    class Reservation : IReservation
+    {
+        public Reservation()
+        {
+
+        }
+        public void MyReservation()
+        {
+            Console.WriteLine($"Booking# {GetCodeBooking()} for {GetDowBooking()}");
+        }
+        public string GetCodeBooking()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            var finalString = new String(stringChars);
+            return finalString;
+        }   
+        public string GetDowBooking()
+        {
+            string[] days = { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+            var random = new Random();         
+            return days[random.Next(days.Length)];          
+        }
+    }
+}
