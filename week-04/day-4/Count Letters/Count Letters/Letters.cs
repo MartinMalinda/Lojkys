ba4a2ffe7859fe5421c19ecff95a904edaa67701
Lukas Lojka <lojkys@gmail.com>
2019-10-03 21:04:33 +0200

    added Count Letters task

---
+++ week-04/day-4/Count Letters/Count Letters/Letters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Letters
+{
+    class Letters
+    {
+        public Letters()
+        {
+
+        }
+        public void CountLetters(string word)
+        {
+            Dictionary<char, int> outputDictionary = new Dictionary<char, int>();
+            //word.Replace(" ", "");
+            foreach (var letter in word)
+            {
+                if (letter == ' ')
+                {
+                    continue;
+                }
+                else if (outputDictionary.ContainsKey(letter))
+                {
+                    outputDictionary[letter]++;
+                }
+                else
+                {
+                    outputDictionary.Add(letter, 1);
+                }
+            }
+            foreach (var item in outputDictionary)
+            {
+                Console.WriteLine(item.Key + " " + item.Value + " ");
+            }
+        }
+    }
+}
---
+++ week-04/day-4/Count Letters/Count Letters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testCount = "kocka leze po zeleze hleda diru, kterou tam vleze";
+            var testObject = new Letters();  
+            
+            testObject.CountLetters(testCount);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
