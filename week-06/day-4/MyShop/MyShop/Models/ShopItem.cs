dfb3ee3ae03965e0f82f95643347382a410ad5d6
Lukas Lojka <lojkys@gmail.com>
2019-10-24 17:33:13 +0200

    add MyShop W6 D4

---
+++ week-06/day-4/MyShop/MyShop/Models/ShopItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class ShopItem
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public int Price { get; }
+        public int Quantity { get; }
+
+        public ShopItem(string name, string description, int price, int quantity)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+
+    }
+}
